3126eec9b5a8007ed81fce08faf2843a5328f0af
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2021-12-21 21:39:55 -0500

    Fixed styling of the blog content

--- web/src/pages/insights/[slug].js
+++ web/src/pages/insights/[slug].js
@@ -2,11 +2,23 @@ import client from '../../utils/client';
 import groq from 'groq';
 import Layout from '../../components/Layout';
 import SectionHero from '../../components/SectionHero';
+import markdownify from '../../utils/markdownify';
 
 const Post = ({ post }) => {
     return (
         <Layout metaTitle="" metaDescription="">
-            <SectionHero title={post.title}></SectionHero>
+            <article className="post post-full">
+                <header className="post-header inner-sm">
+                    <h1 className="post-title underline">{post.title}</h1>
+                </header>
+
+                {post.landscapeImage && (
+                    <div className="post-image">
+                        <img src={post.landscapeImageUrl} alt={post.landscapeImage.alt} />
+                    </div>
+                )}
+                {post.body && <div className="post-content inner-sm">{markdownify(post.body)}</div>}
+            </article>
         </Layout>
     );
 };
@@ -22,7 +34,10 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
     const { slug = '' } = context.params;
-    const post = await client.fetch(groq`*[_type == "post" && slug.current == $slug][0]`, { slug });
+    const post = await client.fetch(
+        groq`*[_type == "post" && slug.current == $slug][0]{..., "mainImageUrl": mainImage.asset->url, "landscapeImageUrl": landscapeImage.asset->url}`,
+        { slug }
+    );
 
     return {
         props: {
---
+++ web/src/utils/markdownify.js
@@ -0,0 +1,10 @@
+import marked from 'marked';
+import htmlToReact from './htmlToReact';
+
+export default function markdownify(markdown) {
+    if (!markdown) {
+        return null;
+    }
+
+    return htmlToReact(marked(markdown));
+}
