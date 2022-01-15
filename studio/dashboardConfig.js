e60e362f388b4b9f9fca68e1a77257f83b1f28e7
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2022-01-15 11:39:17 -0500

    Fix for Netlify Build Hook

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -8,13 +8,13 @@ export default {
                     {
                         title: 'Sanity Studio',
                         apiId: 'a8d0e1fb-ee83-41aa-bf9a-afabb41643f8',
-                        buildHookId: 'https://api.netlify.com/build_hooks/61e2ed098eef0b6e0d256bec',
+                        buildHookId: '61e2ed098eef0b6e0d256bec',
                         name: 'dylanyoung-studio'
                     },
                     {
                         title: 'Website',
                         apiId: 'cbe11e3a-f46c-49a8-9bbd-f6da6e369a63',
-                        buildHookId: 'https://api.netlify.com/build_hooks/61e2eca3f219515d9ff2cab7',
+                        buildHookId: '61e2eca3f219515d9ff2cab7',
                         name: 'dylanyoung-web'
                     }
                 ]
