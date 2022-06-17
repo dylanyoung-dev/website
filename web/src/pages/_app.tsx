d395c2c2bf330d4b47bfb3b40d438127735f127a
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2022-06-17 09:46:48 -0400

    Moved CDP/Personalize Functionality local

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,11 +2,25 @@ import '../sass/main.scss';
 import { useEffect } from 'react';
 import TagManager from 'react-gtm-module';
 import { AppProps } from 'next/app';
+import { initClientScript, sendViewEvent } from 'react-sitecore-personalize';
+import { useRouter } from 'next/router';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter();
     useEffect(() => {
         TagManager.initialize({ gtmId: 'GTM-PK9SFJP' });
+        initClientScript(
+            process.env.SITECORE_CDP_CLIENT_KEY,
+            process.env.SITECORE_CDP_DOMAIN,
+            process.env.SITECORE_CDP_API_ENDPOINT,
+            process.env.SITECORE_CDP_CLIENT_VERSION,
+            process.env.SITECORE_CDP_POS,
+            process.env.SITECORE_CDP_WEBFLOW_TARGET,
+            { LogEvents: true, Currency: 'USD', Language: 'EN', Channel: 'Web' }
+        );
+
+        sendViewEvent(window.location.href);
     }, []);
 
     return <Component {...pageProps} />;
