4ba3ec96bb6f24ddf465e5f176428b64289c9928
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2022-07-01 13:12:54 -0400

    Build Error fixes in Netlify

--- web/next.config.js
+++ web/next.config.js
@@ -13,9 +13,5 @@ module.exports = {
     },
     webpack: (config, { webpack }) => {
         return config;
-    },
-    i18n: {
-        locales: ['en'],
-        defaultLocale: 'en'
     }
 };
--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -1,11 +1,9 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { useEffect } from 'react';
-import TagManager from 'react-gtm-module';
 
 export default class MyDocument extends Document {
     render() {
         return (
-            <Html>
+            <Html lang="en">
                 <Head></Head>
                 <body className="palette-orange style-simple">
                     <Main />
