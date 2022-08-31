4f0f33f2ffdd69ec1361885ade89d8ad9cbbde9d
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2022-08-31 11:02:24 -0400

    next configuration change

--- web/next-sitemap.config.js
+++ web/next-sitemap.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://dylanyoung.dev',
-    generateRobotsTxt: true
+    generateRobotsTxt: true,
+    outDir: './out',
+    sitemapSize: 7000
 };
