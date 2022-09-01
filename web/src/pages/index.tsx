008d076d47f1db1291724b7a15e457b1d5700276
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2022-09-01 15:45:14 -0400

    Adding Meta Description to homepage

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -7,7 +7,10 @@ import groq from 'groq';
 
 const Home = ({ allPosts }) => {
     return (
-        <Layout metaTitle="Dylan Young Developer | Sitecore, .Net, CDP, React, Next.Js" metaDescription="">
+        <Layout
+            metaTitle="Dylan Young Developer | Sitecore, .Net, CDP, Personalize, React, Next.Js"
+            metaDescription="Hello my name is Dylan Young and I'm a Developer Advocate working at Sitecore with a focus on Engagement Cloud. I post content usually atleast once a month or more, and continuing to talk about talks that are exciting to me or the community, as well as my cloud focus at Sitecore."
+        >
             <SectionHero title="Hi, I'm Dylan">Welcome to my personal website, where you can read my latest blog posts and learn more about me</SectionHero>
             <SectionPosts articles={allPosts} title="Latest Posts"></SectionPosts>
         </Layout>
