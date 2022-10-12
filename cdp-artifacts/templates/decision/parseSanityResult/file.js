5cf961e06aaa0252d543e2dd5d4c4a831f0d39b4
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2022-10-12 14:28:29 -0400

    Adding more decision models

---
+++ cdp-artifacts/templates/decision/parseSanityResult/file.js
@@ -0,0 +1,10 @@
+// Use server-side JavaScript to create a programmable template
+// You have full access to the guest context which can be accessed under guest, e.g. guest.email
+// This file is being controlled by Source Control!
+// This file will automatically get combined with your config.json and added to your tenant.
+
+// (function () {
+//     var lastSanityId = getLatestSessionEvents();
+
+//     return lastSanityId;
+// })();
