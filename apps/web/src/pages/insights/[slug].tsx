51497ffcb8170284ea62e2f98f3e07fa674af59b
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2023-12-06 21:23:23 -0500

    Fixing Markdown rendering.  More to do...

--- apps/web/src/pages/insights/[slug].tsx
+++ apps/web/src/pages/insights/[slug].tsx
@@ -1,10 +1,13 @@
-import { Box, Container, Heading, Image, Stack, Text, useColorMode } from '@chakra-ui/react';
+import { Box, Code, Container, Heading, Image, ListItem, OrderedList, Stack, Text, UnorderedList, useColorMode } from '@chakra-ui/react';
 import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
 import groq from 'groq';
 import { LinkedinIcon, LinkedinShareButton, TwitterIcon, TwitterShareButton } from 'next-share';
 import { FC } from 'react';
 import ReactMarkdown from 'react-markdown';
 import Moment from 'react-moment';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { okaidia } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import remarkGfm from 'remark-gfm';
 import { Layout } from '../../components/ui';
 import { IPost } from '../../interfaces';
 import client from '../../utils/client';
@@ -19,45 +22,87 @@ const Post: FC<PostProps> = ({ post, url }: PostProps) => {
     const fullPath = `${url}${shortPath}`;
     const { colorMode } = useColorMode();
 
-    const newTheme = {
-        h1: (props) => {
-            return (
-                <Heading mb={1} as="h1" fontSize="xl">
-                    {props.children}
-                </Heading>
+    const components = {
+        ...ChakraUIRenderer(),
+        h1: ({ node, ...props }) => (
+            <Heading mb={1} as="h1" fontSize="xl" {...props}>
+                {props.children}
+            </Heading>
+        ),
+        h2: ({ node, ...props }) => (
+            <Heading mb={0.5} as="h2" fontSize="lg" {...props}>
+                {props.children}
+            </Heading>
+        ),
+        h3: ({ node, ...props }) => (
+            <Heading mb={0.5} as="h3" fontSize="md" {...props}>
+                {props.children}
+            </Heading>
+        ),
+        code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+                <Box my="4">
+                    <SyntaxHighlighter
+                        style={okaidia}
+                        showLineNumbers
+                        language={match ? [1] : 'typescript'}
+                        PreTag="div"
+                        children={String(children).replace(/\n$/, '')}
+                        {...props}
+                    />
+                </Box>
+            ) : (
+                <Code {...props}>{children}</Code>
             );
         },
-        h2: (props) => {
-            const { children } = props;
-            return (
-                <Heading mb={2} mt={6} lineHeight="none" as="h2" fontSize="lg">
-                    {children}
-                </Heading>
-            );
-        },
-        h3: (props) => {
-            return (
-                <Heading mb={2} mt={6} as="h3" lineHeight="none" fontSize="lg">
-                    {props.children}
-                </Heading>
-            );
-        },
-        pre: (props) => {
-            return (
-                <Box
-                    as="pre"
-                    bg={colorMode == 'dark' ? 'gray.800' : 'gray.900'}
-                    color={'gray.500'}
-                    mb={6}
-                    mt={4}
-                    overflow="auto"
-                    whiteSpace="pre-wrap"
-                    wordBreak="break-word"
-                    {...props}
-                />
-            );
-        }
+        ul: ({ node, ...props }) => <UnorderedList {...props} py={2} pl={10} />,
+        ol: ({ node, ...props }) => <OrderedList {...props} py={2} />,
+        li: ({ node, ...props }) => <ListItem {...props} py="1" />
     };
+
+    // const newTheme = {
+    //     a: ({ node, ...props }) => <Link {...props} isExternal />,
+    //     code({ node, inline, className, children, ...props }) {
+    //         if (inline) {
+    //             return (
+    //                 <Box as="code" whiteSpace="pre-wrap" {...props}>
+    //                     {children}
+    //                 </Box>
+    //             );
+    //         }
+    //     },
+    //     h2: (props) => {
+    //         const { children } = props;
+    //         return (
+    //             <Heading mb={2} mt={6} lineHeight="none" as="h2" fontSize="lg">
+    //                 {children}
+    //             </Heading>
+    //         );
+    //     },
+    //     h3: (props) => {
+    //         return (
+    //             <Heading mb={2} mt={6} as="h3" lineHeight="none" fontSize="lg">
+    //                 {props.children}
+    //             </Heading>
+    //         );
+    //     }
+    //     // pre: (props) => {
+    //     //     return (
+    //     //         <Box
+    //     //             as="pre"
+    //     //             bg={colorMode == 'dark' ? 'gray.800' : 'gray.900'}
+    //     //             color={'gray.500'}
+    //     //             mb={6}
+    //     //             mt={4}
+    //     //             overflow="auto"
+    //     //             whiteSpace="pre-wrap"
+    //     //             wordBreak="break-word"
+    //     //             {...props}
+    //     //         />
+    //     //     );
+    //     // }
+    // };
 
     return (
         <Layout metaTitle={post.title} metaDescription={post.excerpt} ogPhoto={post.landscapeImageUrl} ogUrl={fullPath}>
@@ -88,7 +133,7 @@ const Post: FC<PostProps> = ({ post, url }: PostProps) => {
                     <Stack spacing={{ base: '6', md: '6' }}>
                         {post.body && (
                             <Container size="md">
-                                <ReactMarkdown components={ChakraUIRenderer(newTheme)} skipHtml>
+                                <ReactMarkdown remarkPlugins={[remarkGfm]} components={components} skipHtml>
                                     {post.body}
                                 </ReactMarkdown>
                             </Container>
