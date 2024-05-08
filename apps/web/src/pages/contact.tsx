79a54c3483e4177f3561a04c7780579c0c4adb69
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2024-05-08 11:53:44 -0400

    more responsive styling

--- apps/web/src/pages/contact.tsx
+++ apps/web/src/pages/contact.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Stack } from '@chakra-ui/react';
+import { Box, Container, Heading, Stack, useBreakpointValue } from '@chakra-ui/react';
 import Script from 'next/script';
 import { FC } from 'react';
 import { Layout } from '../components';
@@ -8,28 +8,27 @@ interface ContactPageProps {}
 const ContactPage: FC<ContactPageProps> = ({}) => {
     return (
         <Layout metaTitle="Dylan Young | Contact Me for more information" metaDescription="Leave feedback about my blogs, app projects or general feedback.">
-            <Box as="section" position="relative" w="6xl">
+            <Box as="section" position="relative" w={{ base: 'sm', md: 'md', lg: 'xl', xl: '6xl' }}>
                 <Container py={{ base: '8', md: '12' }} maxW="6xl">
                     <Stack spacing={{ base: '8', md: '8' }}>
-                        <Stack direction="row" justify="space-between" maxW="6xl">
-                            <iframe
-                                data-tally-src="https://tally.so/embed/npOvZ1?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
-                                loading="lazy"
-                                width="100%"
-                                frameborder="0"
-                                marginheight="0"
-                                marginwidth="0"
-                                title="Contact Me"
-                            ></iframe>
+                        <Heading size={useBreakpointValue({ base: 'sm', md: 'md' })}>Contact Me</Heading>
+                        <iframe
+                            data-tally-src="https://tally.so/embed/npOvZ1?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+                            loading="lazy"
+                            width="100%"
+                            frameborder="0"
+                            marginheight="0"
+                            marginwidth="0"
+                            title="Contact Me"
+                        ></iframe>
 
-                            <Script
-                                id="tally-js"
-                                src="https://tally.so/widgets/embed.js"
-                                onLoad={() => {
-                                    window.Tally.loadEmbeds();
-                                }}
-                            />
-                        </Stack>
+                        <Script
+                            id="tally-js"
+                            src="https://tally.so/widgets/embed.js"
+                            onLoad={() => {
+                                window.Tally.loadEmbeds();
+                            }}
+                        />
                     </Stack>
                 </Container>
             </Box>
