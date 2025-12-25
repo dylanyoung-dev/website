5e798ca2e54efdb4b951d6a984063efc5ea7e878
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2025-12-25 08:37:37 -0500

    Fixing sitemap for static export

--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -2,7 +2,7 @@
 const config = {
     siteUrl: process.env.SITE_URL || 'https://dylanyoung.dev',
     generateRobotsTxt: true,
-    outDir: './out',
+    outDir: './public',
     sitemapSize: 7000
 };
 
