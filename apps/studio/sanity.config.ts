3eb4ee5fb9adb13092d76a743a03fa005028dc1e
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2026-03-18 20:49:11 -0400

    Fixing the studio app issues

--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -1,6 +1,6 @@
 import { visionTool } from '@sanity/vision';
 import { defineConfig } from 'sanity';
-import { deskTool } from 'sanity/desk';
+import { structureTool } from 'sanity/structure';
 import { articleCategory, author, page, post, project, series, snippet, snippetCategory, speaking, tagging, videoChannel, videoPost } from './schemas/blog';
 
 export default defineConfig({
@@ -8,7 +8,7 @@ export default defineConfig({
     title: 'DylanYoung.dev',
     projectId: 'lanua4su',
     dataset: 'production',
-    plugins: [deskTool(), visionTool()],
+    plugins: [structureTool(), visionTool()],
     schema: {
         types: [articleCategory, author, page, snippet, snippetCategory, post, series, tagging, videoPost, videoChannel, project, speaking]
     }
