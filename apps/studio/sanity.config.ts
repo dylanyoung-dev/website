d98c2adfc68b5af72dd3fa2f2ce342a461c7343a
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2026-06-22 16:33:15 -0400

    Add sanity-plugin-media to Sanity Studio configuration

--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -1,6 +1,7 @@
 import { visionTool } from '@sanity/vision';
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
+import { media } from 'sanity-plugin-media';
 import { articleCategory, author, page, post, project, series, snippet, snippetCategory, speaking, tagging, videoChannel, videoPost } from './schemas/blog';
 
 export default defineConfig({
@@ -8,7 +9,7 @@ export default defineConfig({
     title: 'DylanYoung.dev',
     projectId: 'lanua4su',
     dataset: 'production',
-    plugins: [structureTool(), visionTool()],
+    plugins: [structureTool(), visionTool(), media()],
     schema: {
         types: [articleCategory, author, page, snippet, snippetCategory, post, series, tagging, videoPost, videoChannel, project, speaking]
     }
